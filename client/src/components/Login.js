3fdbf1ce2ffa1b1a6612d8645c7b1992c931bd19
nyomilm <nyomilm@hotmail.com>
2019-10-12 14:06:11 -0500

    3rd update, login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,62 @@
-import React from "react";
+import React, { useState }  from 'react';
+import { axiosWithAuth } from '../components/axiosWithAuth';
 
-const Login = () => {
+
+
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  });
+
+  const changeHandler = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const submitHandler = e => {
+    e.preventDefault();
+    setCredentials({ username: '', password: '' });
+
+    axiosWithAuth()
+    .post('/login', credentials)
+    .then(res =>{
+      localStorage.setItem('token', res.data.payload);
+      props.history.push('/bubble-page');
+    })
+    .catch(err => console.log(err));
+  };
+
+  
   return (
-    <>
+    <div>
+    
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="username">Username</label>
+        <input>
+        type="text"
+        name="username"
+        value={credentials.username}
+        onChange={changeHandler}
+        </input>
+        <label htmlFor="password">Password</label>
+        <input>
+        type="password"
+        name="password"
+        value={credentials.password}
+        onChange={changeHandler}
+        </input>
+        <button type="submit">Login</button>
+
+      </form>
+    
+
+    </div>
   );
 };
 
---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+//render a route to protect the Bubblespage component
+//checks if the user is authenticated, if they are,
+//it renders the "component" prop. If not it redirects
+//the user to /login.
+const PrivateRoute = ({ component: component, ...rest }) => (
+    <Route
+    {...rest}
+    render={props =>
+    localStorage.getItem('token') ? (
+        <Component {...props} />
+
+    ) : (
+        <Redirect to="/" />
+    )
+    }
+    />
+);
+
+export default PrivateRoute;
